5f23a6c160cf0e213fbba2345f9fae8f96c6fa46
Dodzik <32330727+Dodzik@users.noreply.github.com>
2020-10-20 00:34:22 +0200

    Create students.h

---
+++ Poligon/Tworzenie listy studentow(bląd kompilatora repl.it)/students.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <string>
+
+void AddStudent(int* numberOfStudents,char*** namesList,char*** surnamesList,int** semesterList, char* imiona,char* nazwisko, int numer_sem);
+void PrintListContent (int numberOfStudents,char** surnamesList);
